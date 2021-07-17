948d7d223e7262c80b6641024535dd91030d0821
Bryce <saberfilmsproductions@gmail.com>
2021-07-16 20:57:48 -0400

    Add shutdown sfx

--- hands_up_robot/raspi/soundPlayer.py
+++ hands_up_robot/raspi/soundPlayer.py
@@ -6,15 +6,21 @@ import time
 pygame.mixer.init()
 def get_random_voice(vulgar=False):
     files = []
-    for _, _, f in os.walk("sounds"):
+    for _, _, f in os.walk("hands_up_robot/hands_up_robot/raspi/sounds"):
         for file in f:
             files.append(file)
-    print(files)
     return random.choice(files)
 
 def playSound():
-    sound = pygame.mixer.Sound("sounds/" + get_random_voice())
+    soundName = get_random_voice()
+    sound = pygame.mixer.Sound("hands_up_robot/hands_up_robot/raspi/sounds/" + soundName)
+    print(f"[#] Playing {soundName}")
     sound.set_volume(1)
-    print(sound.get_volume())
+
+    sound.play(0)
+    time.sleep(sound.get_length())
+
+def playShutdownSound():
+    sound = pygame.mixer.Sound("hands_up_robot/hands_up_robot/raspi/SHUTDOWN.wav")
     sound.play(0)
     time.sleep(sound.get_length())
