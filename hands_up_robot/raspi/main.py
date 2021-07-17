31f6c3d9eed369de87c7af61339164bd2ca1d3fa
Bryce <saberfilmsproductions@gmail.com>
2021-07-16 20:56:07 -0400

    Final Raspi Code

--- hands_up_robot/raspi/main.py
+++ hands_up_robot/raspi/main.py
@@ -1,20 +1,55 @@
 import os
 import soundPlayer
 import serial
-from threading import Timer, Thread
-from time import time
+import time
+import sys
+import datetime
 
-serial = serial.Serial("/dev/ttyACM0", 9600)
+with open("logs.txt", "a") as f:
+    f.write(f"[#] STARTED - {time.time()}")
 
-hands_down = True
-delay_interval = 500
+serial = serial.Serial("/dev/ttyACM0", 9600, timeout=0)
 
-strings = ['handsup', 'handsdown']
+hands_up = False
+delay_interval = 5
+strings = [b"handsup", b"handsdown"]
 
 def on_hands_down():
+    print('KEEP YA HANDS UP')
     soundPlayer.playSound()
 
+vulgar = False
 loop = True
+timer = 0
+shutdown_timer = 0
+shutdown_time = 20
+timer_length = 5
+print("[!] Starting Up...")
+
 while loop:
+    os.system('clear')
     line = serial.readline()
-    
+    print("[~] Read Line: ", end="")
+    print(line)
+    timer += 0.3
+    if shutdown_timer >= shutdown_time:
+        print("SHUTTING DOWN")
+        sys.exit()
+    if timer >= timer_length:
+        on_hands_down()
+        timer = 0
+    if strings[0] in line:
+        hands_up = True
+    elif strings[1] in line:
+        hands_up = False
+    print(f"[#] hands_up: {hands_up}")
+    if hands_up:
+        shutdown_timer += 0.3
+        timer = 0
+    if not hands_up:
+        shutdown_timer = 0
+    print(f"[#] Timer: {timer}")
+    print(f"[#] ShutDown Timer: {shutdown_timer}")
+
+    time.sleep(0.1)
+   
